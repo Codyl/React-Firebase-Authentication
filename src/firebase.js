8b8596531790bd30aa3a42500cccf67571792e28
Cody Lillywhite <codylillyw@gmail.com>
2021-09-09 19:09:51 -0400

    sec

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,14 @@ import 'firebase/compat/auth';
 // import {useAuthState} from 'react-firebase-hooks/auth';
 // import {useCollectionsData} from 'react-firebase-hooks/firestore';
 
-const app = firebase.initializeApp({
-    apiKey: "AIzaSyA_ccJxMhr_KNWSSIpnCadV2hRZFfXOLjQ",
-    authDomain: "fanowrimo-development.firebaseapp.com",
-    projectId: "fanowrimo-development",
-    storageBucket: "fanowrimo-development.appspot.com",
-    messagingSenderId: "399541799846",
-    appId: "1:399541799846:web:6400fb5e6cf1c6bc29dd67"
-});
+const appConfig = {
+    apiKey: 'AIzaSyA_ccJxMhr_KNWSSIpnCadV2hRZFfXOLjQ',
+    authDomain: process.env.BOOK_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.BOOK_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.BOOK_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.BOOK_FIREBASE_SENDER_ID,
+    appId: process.env.BOOK_FIREBASE_APP_ID
+};
+const app = firebase.initializeApp(appConfig);
 export const auth = app.auth();
 export default app;
